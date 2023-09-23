473ee8362d4b52a210a0adf62b91e57292383bfe
donlouie <don_gallegos@outlook.com>
2023-09-23 20:32:10 +0800

    update 9.23.23

---
+++ Codewars/7 kyu/how-many-twos.js
@@ -0,0 +1,24 @@
+/*
+Write a function that returns the number of '2's in the factorization of a number.
+For example,
+
+twoCount(24)
+should return 3, since the factorization of 24 is 2^3 x 3
+
+twoCount(17280)
+should return 7, since the factorization of 17280 is 2^7 x 5 x 3^3
+The number passed to two_count (twoCount) will always be a positive integer greater than or equal to 1.
+*/
+
+const twoCount = (n) => {
+	let counter = 0;
+
+	while (n % 2 === 0) {
+		n /= 2;
+		counter += 1;
+	}
+
+	return counter;
+};
+
+console.log(twoCount(24)); // => 3

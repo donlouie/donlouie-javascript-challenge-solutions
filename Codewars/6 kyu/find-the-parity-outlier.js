04aa944c6c026f849792438934a61b41c2d6bc93
donlouie <don_gallegos@outlook.com>
2023-12-06 18:29:29 +0800

    add hackerearth challenges

--- Codewars/6 kyu/find-the-parity-outlier.js
+++ Codewars/6 kyu/find-the-parity-outlier.js
@@ -24,3 +24,11 @@ const findOutlier = (integers) => {
 
 console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36])); // -->  11 (the only odd number)
 // console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21])); // --> 160
+
+// Other Solution
+// Solution 1
+function findOutlier(int) {
+	var even = int.filter((a) => a % 2 == 0);
+	var odd = int.filter((a) => a % 2 !== 0);
+	return even.length == 1 ? even[0] : odd[0];
+}

4f0ec580b74d6e7ce2bac8b76d1a7268299ca8eb
donlouie <don_gallegos@outlook.com>
2023-12-11 18:58:34 +0800

    test commit

---
+++ leetcode/2634 filter elements from array.js
@@ -0,0 +1,12 @@
+/*
+https://leetcode.com/problems/filter-elements-from-array/submissions/1115164223/?envType=study-plan-v2&envId=30-days-of-javascript
+*/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+	return arr.filter(fn);
+};
